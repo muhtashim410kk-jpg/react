bfa506b7b228943f257819f75b253ce020872200
Muhtashim <muhtashim410kk@gmail.com>
2026-08-04 16:42:44 +0500

    Conditions using ?

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-
-function App(){
-
-   return(
-
-      
-   
-      <div>
-            <h1>Muhtashim</h1>
-      </div>
-   
+ import { useState } from "react";
 
 
-   )
+ function App(){
+
+    const [count,setcount]=useState(0)
+
+    return(
+                
+           <div>
+              <button onClick={()=>setcount(count+1)} >Increment</button>
+              <h1>{count}</h1>
+              {
+               count==0? <h1>muhtashim0</h1>
+               :count==1? <h1>muhtashim1</h1>
+               :count==2? <h1>muhtashim2</h1>
+               :count==3? <h1>muhtashim3</h1>
+               :count==4? <h1>muhtashim4</h1>
+               :null
+                             
+              }
+
+           </div>
 
 
-}
+    )
 
-export default App
+ }
+
+ export default App
--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,9 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App'
 
-import Jsxhandling from './component'
-import Counter from './component'
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Counter />
+  <StrictMode>    
     <App />
-  </StrictMode>,
+  </StrictMode>
 )

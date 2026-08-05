b4db400e67fc291cecdf6452773942216a0b26cc
Muhtashim <muhtashim410kk@gmail.com>
2026-08-06 02:41:24 +0500

    Form with handling input values

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
+import { useState } from "react"
 
 
 function Handlinginput(){
         
-
+          const [name,setname]= useState("");
+          const [email,setemail] = useState(""); 
+          const [Password,setpassword]=useState(""); 
    
  return(
 
-         <div>
-                
-              <input  onChange={(event)=> alert(event.target.value)} type="text" placeholder="Enterusername"  />
+         <div style={{backgroundColor:"gray"}}   > 
+                     <h1 style={{textAlign:"center"}} >SIMPLE FORM</h1>
+                      
+ <input  style={{margin:"12px", marginLeft:"800px"  }} onChange={(event)=> setname(event.target.value)} type="text" placeholder="Enterusername" id="username" value={name} />Username
+            <label htmlFor="username"></label>
+      <br />
+<input   style={{margin:"12px" , marginLeft:"800px" }} onChange={(event)=> setemail(event.target.value)} type="text" placeholder="Enteremail" id="Email" value={email} />Email
+            <label htmlFor="Email"></label>
+
+ <input   style={{margin:"12px" , marginLeft:"800px" }} onChange={(event)=> setpassword(event.target.value)} type="text" placeholder="Enterpassword" id="Password" value={Password} />Password
+            <label htmlFor="Password"></label>       
+
+  <h1  style={{marginLeft:"800px"}} >  Name:  {name}   </h1>          
+  <h1  style={{marginLeft:"800px"}} >   Email:  {email}   </h1> 
+  <h1  style={{marginLeft:"800px"}} >   Password:  {Password}   </h1>    
+
+           <button style={{marginLeft:"870px" , backgroundColor:"white", padding:"12px" }} >SUBMIT FORM</button>      
 
          </div>
         

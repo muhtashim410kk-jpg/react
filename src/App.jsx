99e7fa4c9eaf647ae82ca380e0b650a6e7b7ec54
Muhtashim <muhtashim410kk@gmail.com>
2026-08-07 20:00:22 +0500

    use of useeffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,77 @@
+// import { useState } from "react";
 
-   function Loopjsx(){
-             
-     let userdata =[
+// function Useffect(){
 
-      {
-        "Id":123,
-        "Name":"muhtashim",
-        "Class":"bs-it",
-        "roll no":123
+//   const [counter,setcounter]=useState(0)
 
-      },
-         {
-        "Id":123,
-        "Name":"tooba",
-        "Class":"bs-it",
-        "roll no":123
+//   function oncecall(){
+//     console.log("muhtashim");
+    
+//   }
 
-      },
-         {
-        "Id":123,
-        "Name":"anas",
-        "Class":"bs-it",
-        "roll no":123
+//   oncecall();  
 
-      },
-         {
-        "Id":123,
-        "Name":"asad",
-        "Class":"bs-it",
-        "roll no":123
+// return(
 
-      },
+//   <div>
+//     {/* so jb bhi hum counter pr click kr rahe hain to ye function again and again call ho raha ha
+//     yani agar function ma koi zyda code hota ya hum zyda chezein kr rahe hote to pehle wo function
+//     bhi call hona tha sara code run hona tha to performance low ho jani thi to hum chahte hain ke function
+//     aik bari call ho to uske lia hum useeffect ka use krein ge   agar hum dependency array ko khali chor dein ge
+//     to aik bar run hoga or agar usme state ki value dalien ge to tb run hoga jb state change hogi just tb
+//     function run hoga */}
+//              <button onClick={()=>setcounter(counter+1)} >Counter:{counter}</button>
+//         <h1>Muhtashim</h1>
+
+//   </div>
+// )
 
 
 
-     ]     
 
-    return(
-
-   <div>
-
-                <h1>LOOP with jsx Data</h1>
-         <table border="1">
-
-       <thead>
-
-           <tr>
-            <td>ID</td>
-            <td>Name</td>
-            <td>Class</td>
-            <td>Roll no</td>
-            
-           </tr>
-
-       </thead>
-
-       <tbody>
-
-            {
-              userdata.map((user)=>{
-
-          return(
-                   <tr>
-            <td>{user.Id}</td>
-            <td>{user.Name}</td>
-            <td>{user.Class}</td>
-            <td>{user["roll no"]}</td>
-            
-           </tr>
-          )
-
-              })
-            }
+// }
+ 
+// export default Useffect
 
 
-       </tbody>
+
+import { useEffect, useState } from "react";
+
+function Useffect(){
+
+  const [counter,setcounter]=useState(0)
+  const [data,setdata]=useState(0)
+
+  useEffect(()=>{
 
 
-         </table>
+ oncecall();
+// ab humne useeffect ki dependancy array may data dala ha to ab function ki call data pr depend kre gi
+// jb data pr click hoga function again and again call hoga but counter ko humne ni dala to us pr click
+// se call ni hoga
 
-     
-   </div>        
 
-    )
+  },[data])
 
-   }
+  function oncecall(){
+    console.log("muhtashim");
+    
+  }
+ 
 
-   export default Loopjsx
+return(
+
+  <div>
+             <button onClick={()=>setdata(data+1)} >Data:{data}</button>
+             <button onClick={()=>setcounter(counter+1)} >Counter:{counter}</button>
+        <h1>Muhtashim</h1>
+
+  </div>
+)
+
+
+
+
+}
+ 
+export default Useffect
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Loopjsx from './App'
+import Useffect from './App'
+
 
 
 
@@ -11,6 +12,6 @@ import Loopjsx from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>    
-    <Loopjsx />
+    <Useffect />
   </StrictMode>
 )
